5f2336acd4c978228746e7ddb23f9ee8de21ea9f
Sujin <sujin.2f@gmail.com>
2018-05-18 02:05:31 -0400

    Server Side Rendering

--- src/web/app.js
+++ src/web/app.js
@@ -1,27 +1,31 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
-import Root from 'components/Root';
 import configureStore, { history } from 'store';
+import { AppContainer } from 'react-hot-loader';
+
+import Root from 'src/components/Root';
 
 const store = configureStore();
+
+console.log(process.env);
 
 render(
   <AppContainer>
     <Root store={store} history={history} />
   </AppContainer>,
-  document.getElementById('root')
+  document.getElementById('app'),
 );
 
 if (module.hot) {
-  module.hot.accept('./components/Root', () => {
-    const NextRoot = require('./components/Root'); // eslint-disable-line global-require
+  module.hot.accept('src/components/Root', () => {
+    const NextRoot = require('src/components/Root'); // eslint-disable-line global-require
 
     render(
       <AppContainer>
         <NextRoot store={store} history={history} />
       </AppContainer>,
-      document.getElementById('root')
+      document.getElementById('app'),
     );
   });
 }
+
